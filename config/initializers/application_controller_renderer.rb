019853d22c023ac5bcdafb3ed750179963b739b9
鈴木年章 <tsakszk@suzukinenshounoMacBook-puro.local>
2020-03-22 15:20:05 +0900

    Initial commit1

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'ea090e1e3f2f43746855f5cdd4fafb4f6fa0492750ec0a648ec19f54e34a037be47c88532dc2cd90c61acf9ea3b3e89e1e539a626861476e58d3964ec736736c'
